36ce025ed6686487c50a38d0cba99e475e56e778
djurakulov14 <109537648+djurakulov14@users.noreply.github.com>
2023-01-24 17:46:33 +0500

    added new skills

--- src/components/FourthSection.jsx
+++ src/components/FourthSection.jsx
@@ -9,14 +9,16 @@ export const FourthSection = () => {
             <Skill><h1>HTML5</h1></Skill>
             <Skill><h1>CSS3</h1></Skill>
             <Skill><h1>SASS/SCSS</h1></Skill>
-            <Skill><h1>JavaScript</h1></Skill>
-            <Skill><h1>Axios</h1></Skill>
-            <Skill><h1>Git/Github</h1></Skill>
-            <Skill><h1>React.js</h1></Skill>
-            <Skill><h1>Redux.toolkit</h1></Skill>
             <Skill><h1>Tailwind</h1></Skill>
             <Skill><h1>Material UI</h1></Skill>
             <Skill><h1>Flowbite</h1></Skill>
+            <Skill><h1>JavaScript</h1></Skill>
+            <Skill><h1>React.js</h1></Skill>
+            <Skill><h1>Redux.toolkit</h1></Skill>
+            <Skill><h1>Next.js</h1></Skill>
+            <Skill><h1>Typescript</h1></Skill>
+            <Skill><h1>Axios</h1></Skill>
+            <Skill><h1>Git/Github</h1></Skill>
             <Skill><h1>English</h1></Skill>
         </div>
     </section>
